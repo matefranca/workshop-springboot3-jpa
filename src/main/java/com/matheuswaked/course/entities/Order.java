c7a0c5741b81a7b7bc33e02f27a5e4d61efee6d0
Matheus Waked Franca <matheus363@hotmail.com>
2023-03-09 17:09:24 -0300

    Subtotal & Total methods

--- src/main/java/com/matheuswaked/course/entities/Order.java
+++ src/main/java/com/matheuswaked/course/entities/Order.java
@@ -101,6 +101,12 @@ public class Order implements Serializable {
 	public Set<OrderItem> getItems() {
 		return items;
 	}
+	
+	public Double getTotal() {		
+		return items.stream()
+				.map(x -> x.getSubTotal())
+				.reduce(0.0, (x,y) -> x + y);
+	}
 
 	@Override
 	public int hashCode() {
