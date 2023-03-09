899733360947bb4178437d906ec8f58c36549e6b
Matheus Waked Franca <matheus363@hotmail.com>
2023-03-09 05:00:01 -0300

    OrderItem, many-to-many association with extra attributes

---
+++ src/main/java/com/matheuswaked/course/repositories/OrderItemRepository.java
@@ -0,0 +1,9 @@
+package com.matheuswaked.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.matheuswaked.course.entities.OrderItem;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long>{
+
+}
